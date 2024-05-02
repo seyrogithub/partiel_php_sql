09ace6def0e58e8cbefc81a2f299626756a5bc61
seyrogithub <144135572+seyrogithub@users.noreply.github.com>
2024-05-02 13:26:27 +0200

    partie2

--- partiel_web.sql
+++ partiel_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : jeu. 02 mai 2024 à 11:09
+-- Généré le : jeu. 02 mai 2024 à 11:25
 -- Version du serveur : 5.7.24
 -- Version de PHP : 8.0.1
 
@@ -31,11 +31,22 @@ CREATE TABLE `film` (
   `id` int(10) NOT NULL,
   `duree` int(10) NOT NULL,
   `titre` varchar(50) NOT NULL,
-  `description` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
   `date_de_sortie` date NOT NULL,
   `id_genre` int(10) NOT NULL,
   `id_réalisateur` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `film`
+--
+
+INSERT INTO `film` (`id`, `duree`, `titre`, `description`, `date_de_sortie`, `id_genre`, `id_réalisateur`) VALUES
+(1, 169, 'Interstellar', 'Le film raconte les aventures d’un groupe d’explorateurs qui utilisent une faille récemment découverte dans l’espace-temps afin de repousser les limites humaines et partir à la conquête des distances astronomiques dans un voyage interstellaire.', '2014-11-05', 1, 1),
+(2, 164, 'Django Unchained', 'Un chasseur de primes et un esclave noir s\'associent pour retrouver la femme de ce dernier, retenue en esclavage par le propriétaire d\'une plantation.', '2013-01-16', 2, 2),
+(3, 148, 'Inception', 'Au lieu de subtiliser un rêve, un voleur expérimenté et son équipe doivent faire l\'inverse : implanter une idée dans l\'esprit d\'un individu. S\'ils y parviennent, il pourrait s\'agir du crime parfait ...', '2010-07-21', 1, 1),
+(4, 105, 'Coco', 'Depuis déjà plusieurs générations, la musique est bannie dans la famille de Miguel. Un vrai déchirement pour le jeune garçon dont le rêve ultime est de devenir un musicien aussi accompli que son idole, Ernesto de la Cruz.', '2017-11-15', 3, 3),
+(5, 122, 'Joker', 'Le film, qui relate une histoire originale inédite sur grand écran, se focalise sur la figure emblématique de l’ennemi juré de Batman. Il brosse le portrait d’Arthur Fleck, un homme sans concession méprisé par la société.', '2019-10-09', 4, 4);
 
 -- --------------------------------------------------------
 
@@ -47,6 +58,16 @@ CREATE TABLE `genre` (
   `id` int(10) NOT NULL,
   `libelle` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `genre`
+--
+
+INSERT INTO `genre` (`id`, `libelle`) VALUES
+(1, 'Science Fiction'),
+(2, 'Western'),
+(3, 'Animation'),
+(4, 'Drame');
 
 -- --------------------------------------------------------
 
@@ -60,6 +81,16 @@ CREATE TABLE `realisateur` (
   `prenom` varchar(50) NOT NULL,
   `naionalite` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `realisateur`
+--
+
+INSERT INTO `realisateur` (`id`, `nom`, `prenom`, `naionalite`) VALUES
+(1, 'Nolan', 'Christopher', 'Britannique'),
+(2, 'Tarantino', 'Quentin', 'Americaine'),
+(3, 'Molina', 'Adrian', 'Mexicaine'),
+(4, 'Phillips', 'Todd', 'Americaine');
 
 --
 -- Index pour les tables déchargées
@@ -93,19 +124,19 @@ ALTER TABLE `realisateur`
 -- AUTO_INCREMENT pour la table `film`
 --
 ALTER TABLE `film`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `genre`
 --
 ALTER TABLE `genre`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `realisateur`
 --
 ALTER TABLE `realisateur`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Contraintes pour les tables déchargées
