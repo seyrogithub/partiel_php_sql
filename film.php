e5e12ffd6b0488ce35e2d7f83e7990140ccfe026
seyrogithub <144135572+seyrogithub@users.noreply.github.com>
2024-05-02 14:14:08 +0200

    debut partie 4

---
+++ film.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('index.php');
+
+$id_film = $_GET['id_film'];
+
+echo "$id_film";
+
+?>
+
--- index.php
+++ index.php
@@ -16,7 +16,7 @@ try {
 echo "<h2> Liste des meilleurs films des années 2010 : </h2>";
 
 $resultat = $dbPDO->prepare("
-SELECT YEAR(date_de_sortie) AS date, titre, g.libelle AS genre, r.prenom AS prenom, r.nom AS nom  FROM film f
+SELECT YEAR(date_de_sortie) AS date, titre, g.libelle AS genre, r.prenom AS prenom, r.nom AS nom, f.id AS id FROM film f
 INNER JOIN genre g ON  f.id_genre = g.id
 INNER JOIN realisateur r ON f.id_réalisateur = r.id;
 ");
@@ -24,8 +24,10 @@ $resultat->execute();
 $film = $resultat->fetchAll(PDO::FETCH_CLASS);
 
 foreach ($film as $films) {
-    echo "<ul><li>$films->titre ($films->genre de $films->prenom $films->nom, $films->date)</li></ul>";
+    echo "<ul><li><a href='film.php?id_film=$films->id'>$films->titre</a> ($films->genre de $films->prenom $films->nom, $films->date)</li></ul>";
 }
+
+
 
 ?>
 
